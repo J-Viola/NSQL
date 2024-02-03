fd9b227078202fbf4a461abdca8330ec1269eaf3
J-Viola <honza.viola@email.cz>
2024-02-03 12:42:40 +0100

    Added secret key

--- code/main.py
+++ code/main.py
@@ -3,7 +3,7 @@ import pymongo
 
 
 app = Flask(__name__)
-
+app.secret_key = "super secret key"
 mongo_client = pymongo.MongoClient("mongodb://admin:admin@mongodb:27017", connect=False)
 
 if __name__ == '__main__':
